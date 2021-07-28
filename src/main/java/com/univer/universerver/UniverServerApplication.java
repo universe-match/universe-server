0e3056965349f353fded0a4aa67428a3bf9f992c
soulchoiseongjun <csj2702@soulenergy.co.kr>
2021-07-28 09:48:50 +0900

    init

---
+++ src/main/java/com/univer/universerver/UniverServerApplication.java
@@ -0,0 +1,13 @@
+package com.univer.universerver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniverServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniverServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/univer/universerver/UniverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univer.universerver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniverServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
